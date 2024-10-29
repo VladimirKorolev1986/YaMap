098b2d091a088f2ec76bef7be231ff067e7d21b0
Владимир Королев <vladimirkorolev@MacBook-Air-Vladimir.local>
2024-10-29 21:01:46 +0300

    add icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
+let center = [55.74074112342451, 37.99879782368382]
+
 function init() {
     let map = new ymaps.Map('map-test', {
-        center: [55.74074112342451,37.99879782368382],
+        center: center,
         zoom: 15
     })
-}
 
+
+    let placemark = new ymaps.Placemark(center, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/location-pin.png',
+        IcomImageSize: [20, 20],
+        IconImageOffset: [0, 0]
+    })
+
+    map.geoObjects.add(placemark)
+}
 ymaps.ready(init);
 
